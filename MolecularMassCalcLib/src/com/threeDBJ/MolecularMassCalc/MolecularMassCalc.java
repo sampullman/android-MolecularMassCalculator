e59ec107bf317de72b485d28dba24f75a04a8dae
Sam Pullman <sam@comp2.(none)>
2012-11-18 17:34:10 -0800

    added readme

--- MolecularMassCalcLib/src/com/threeDBJ/MolecularMassCalc/MolecularMassCalc.java
+++ MolecularMassCalcLib/src/com/threeDBJ/MolecularMassCalc/MolecularMassCalc.java
@@ -215,7 +215,8 @@ public class MolecularMassCalc extends Activity {
 	    } else {
 		resHolder.setVisibility(View.INVISIBLE);
 	    }
-	    resultsVisible = false;	}
+	    resultsVisible = false;
+	}
     }
 
     private OnClickListener calcBtn = new OnClickListener() {
